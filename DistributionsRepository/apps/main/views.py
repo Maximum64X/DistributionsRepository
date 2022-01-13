179358e3c1120f4c8a79b0caa6bdb02221788509
Maximum64X <maksim.fedosov.2002@icloud.com>
2022-01-14 00:00:20 +0300

    Now the project can read catalogs and files, and their modification date in media catalog (not in subcatalogs yet)

--- DistributionsRepository/apps/main/views.py
+++ DistributionsRepository/apps/main/views.py
@@ -1,4 +1,17 @@
+import os, datetime
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/main.html')
+
+    os.chdir("DistributionsRepository/media")
+    folders = os.listdir()
+
+    modification_time = []
+    for folder in folders:
+        modification_time.append(datetime.datetime.fromtimestamp(os.stat(folder).st_mtime).strftime('%d.%m.%Y %H:%M'))
+
+    os.chdir('../..')
+
+    folders = dict(zip(folders, modification_time))
+
+    return render(request, 'main/main.html', {"folders": folders})
